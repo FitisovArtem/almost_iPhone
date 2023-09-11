2483ab39bd784aa91c3157180f7dca740c3de009
FitisovArtem <109963097+FitisovArtem@users.noreply.github.com>
2023-09-11 22:45:54 +0300

    translate to ukr

---
+++ Notes/main2.py
@@ -0,0 +1,10 @@
+def number_one(text):
+    print(f"number_one: {text}")
+
+
+def number_two(text):
+    print(f"number_two: {text}")
+
+
+def number_three(text):
+    print(f"number_three: {text}")
