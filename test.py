ca3bff2402d9b58befb2cafba320db8342587c59
FitisovArtem <109963097+FitisovArtem@users.noreply.github.com>
2023-09-15 14:35:20 +0300

    ver 1.0.0

---
+++ test.py
@@ -0,0 +1,9 @@
+import re
+
+email = 'my_email@outlook.co'
+regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
+
+if re.fullmatch(regex, email):
+    print("Valid email")
+else:
+    print("Invalid email")
